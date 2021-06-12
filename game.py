a1d450173123e3813fe56cc69e3f68a6b9d05921
Sourabh Sathe <sou22600@gmail.com>
2021-06-12 22:46:54 +0530

    Fixed Codefactor issues

--- game.py
+++ game.py
@@ -1,6 +1,10 @@
-import pygame, sys, random
-from pygame.locals import *
+import pygame
+import sys
+import random
 from tkinter import messagebox, Tk
+
+from pygame.constants import MOUSEBUTTONDOWN, QUIT
+
 
 Tk().withdraw()
 screenWidth = 800
@@ -26,16 +30,22 @@ red_cross = pygame.image.load("assets/red_cross.png").convert_alpha()
 
 duckCoor = [(350, 390), (450, 390), (550, 390), (650, 390)]
 
-duckInitialx = 0
-duckInitialy = 0
-direction = "up"
-duckx = 0
-ducky = 0
-choice = 0
-popupSpeed = 100
-score = 0
-cross = 0
-clicked = False
+
+class Global:
+    def __init__(self):
+        self.duckInitialx = 0
+        self.duckInitialy = 0
+        self.direction = "up"
+        self.duckx = 0
+        self.ducky = 0
+        self.choice = 0
+        self.popupSpeed = 100
+        self.score = 0
+        self.cross = 0
+        self.clicked = False
+
+
+Global = Global()
 
 
 def refresh():
@@ -44,39 +54,41 @@ def refresh():
 
 
 def blitCross():
-    global cross, score
-    if 1 <= cross <= 3:
+    # global cross, score
+    if 1 <= Global.cross <= 3:
         SCREEN.blit(red_cross, (screenWidth - 165, 55))
-    if 2 <= cross <= 3:
+    if 2 <= Global.cross <= 3:
         SCREEN.blit(red_cross, (screenWidth - 165 + 47, 55))
-    if cross >= 3:
+    if Global.cross >= 3:
         SCREEN.blit(red_cross, (screenWidth - 165 + 47 + 47, 55))
         refresh()
-        messagebox.showinfo(title="Game Over", message=f"Game over\nScore: {score}")
+        messagebox.showinfo(
+            title="Game Over", message=f"Game over\nScore: {Global.score}"
+        )
         pygame.quit()
         sys.exit()
     refresh()
 
 
 def mousePosition():
-    global choice
+    # global choice
     mousex, mousey = pygame.mouse.get_pos()
-    if choice == 0:
+    if Global.choice == 0:
         if (duckCoor[0][0] <= mousex <= (duckCoor[0][0] + 90)) and (
             (duckCoor[0][1] - 130) <= mousey <= (duckCoor[0][1] - 5)
         ):
             return True
-    elif choice == 1:
+    elif Global.choice == 1:
         if (duckCoor[1][0] <= mousex <= (duckCoor[1][0] + 90)) and (
             (duckCoor[0][1] - 130) <= mousey <= (duckCoor[0][1] - 5)
         ):
             return True
-    elif choice == 2:
+    elif Global.choice == 2:
         if (duckCoor[2][0] <= mousex <= (duckCoor[2][0] + 90)) and (
             (duckCoor[0][1] - 130) <= mousey <= (duckCoor[0][1] - 5)
         ):
             return True
-    elif choice == 3:
+    elif Global.choice == 3:
         if (duckCoor[3][0] <= mousex <= (duckCoor[3][0] + 90)) and (
             (duckCoor[0][1] - 130) <= mousey <= (duckCoor[0][1] - 5)
         ):
@@ -85,76 +97,73 @@ def mousePosition():
 
 
 def check_score():
-    global popupSpeed
-    if 5 <= score < 10:
-        popupSpeed = 50
-    elif 10 <= score < 20:
-        popupSpeed = 20
+    # global popupSpeed
+    if 5 <= Global.score < 10:
+        Global.popupSpeed = 50
+    elif 10 <= Global.score < 20:
+        Global.popupSpeed = 20
 
 
 def randomDuck():
-    global duckInitialx, duckInitialy, duckx, ducky, choice
+    # global duckInitialx, duckInitialy, duckx, ducky, choice
     # choice=3
-    choice = random.choice([0, 1, 2, 3])
+    Global.choice = random.choice([0, 1, 2, 3])
     # print(f"\t\t\t{choice} ")
-    duckInitialx = duckCoor[choice][0]
-    duckInitialy = duckCoor[0][1]
-    duckx = duckInitialx
-    ducky = duckInitialy
+    Global.duckInitialx = duckCoor[Global.choice][0]
+    Global.duckInitialy = duckCoor[0][1]
+    Global.duckx = Global.duckInitialx
+    Global.ducky = Global.duckInitialy
 
 
 def blitOnScreen():
-    global duckx, ducky, score, cross
+    # global duckx, ducky, score, cross
     # mousex, mousey = pygame.mouse.get_pos()
 
     SCREEN.blit(background, (0, 0))
-    SCREEN.blit(duckPic, (duckx, ducky))
+    SCREEN.blit(duckPic, (Global.duckx, Global.ducky))
     SCREEN.blit(background_overlay, (0, 0))
     SCREEN.blit(scoreimg, (screenWidth - 220, 0))
     font = pygame.font.Font(None, 60)
-    myScore = font.render(str(score), 1, (84, 58, 36))
+    myScore = font.render(str(Global.score), 1, (58, 31, 4))
     SCREEN.blit(myScore, (screenWidth - 80, 8))
     # SCREEN.blit(crosshair, (mousex-(crosshair.get_width()/2),mousey-(crosshair.get_height()/2)))
     blitCross()
 
 
 def up():
-    global direction, duckx, ducky, duckInitialx, duckInitialy
-    while direction != "down":
+    # global direction, duckx, ducky, duckInitialx, duckInitialy
+    while Global.direction != "down":
         # popupSpeed=check_score()
-        if direction == "up":
-            ducky -= 10
-            if ducky <= duckInitialy - 120:
+        if Global.direction == "up":
+            Global.ducky -= 10
+            if Global.ducky <= Global.duckInitialy - 120:
                 # duckVisible=True
-                direction = "down"
+                Global.direction = "down"
         blitOnScreen()
 
 
 def down():
-    global direction, duckx, ducky, duckInitialx, duckInitialy, clicked, cross, score
-    while direction != "up":
-        if direction == "down":
-            ducky += 10
-            if ducky >= duckInitialy:
+    # global direction, duckx, ducky, duckInitialx, duckInitialy, clicked, cross, score
+    while Global.direction != "up":
+        if Global.direction == "down":
+            Global.ducky += 10
+            if Global.ducky >= Global.duckInitialy:
                 # duckVisible=False
-                direction = "up"
+                Global.direction = "up"
         blitOnScreen()
 
 
 def delay_loop():
-    global popupSpeed, score, clicked, cross
-    x = 0
-    while not x >= popupSpeed:
+    for _ in range(Global.popupSpeed):
         check_score()
-        x += 1
-        # print(x)
-        clicked = False
+
+        Global.clicked = False
         for event in pygame.event.get():
             if event.type == MOUSEBUTTONDOWN and event.button == 1 and mousePosition():
                 print("click")
-                score += 1
-                clicked = True
-                return clicked
+                Global.score += 1
+                Global.clicked = True
+                return
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
@@ -169,8 +178,8 @@ if __name__ == "__main__":
         up()
         delay_loop()
         down()
-        if not clicked:
+        if not Global.clicked:
             # score-=1
-            cross += 1
+            Global.cross += 1
             # blitCross()
         delay_loop()
